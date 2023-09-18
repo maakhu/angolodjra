396774300a427a03c372e388c0f0f09de8d1a69f
maakhu <egri@me.com>
2023-09-18 19:00:18 +0200

    add homepage finalisation, add animations

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -7,6 +7,7 @@ import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import { motion } from "framer-motion";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -21,28 +22,50 @@ const Home = ({setSelectedPage}: Props) => {
       className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0"
     >
       {/* IMAGE AND MAIN HEADER */}
-      <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
+      <motion.div 
+        className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6"
+        onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+      >
         {/* MAIN HEADER */}
         <div className="z-10 mt-32 md:basis-3/5">
           {/* HEADINGS */}
-          <div>
-            <div>
-              <div>
+          <motion.div 
+            className="md:-mt-20"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 }
+            }}
+          >
+            <div className="relative">
+              <div className="before:absolute  before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
                 <img src={HomePageText} alt="home-page-text" />
               </div>
             </div>
-            <p className="text-3xl font-bold text-gray-100">
+            <p className="mt-8 text-3xl font-bold text-gray-100">
               <span>
                 The best way to
-              <br />
                 to learn
               <br />
                 English
               </span>
             </p>
-          </div>
+          </motion.div>
           {/* ACTIONS */}
-          <div>
+          <motion.div
+           className="mt-8 flex items-center gap-8"
+           initial="hidden"
+           whileInView="visible"
+           viewport={{ once: true, amount: 0.5 }}
+           transition={{ delay: 0.2, duration: 0.5 }}
+           variants={{
+             hidden: { opacity: 0, x: -50 },
+             visible: { opacity: 1, x: 0 }
+           }}
+           >
             <ActionButton setSelectedPage={setSelectedPage}>
               Get started
             </ActionButton>
@@ -53,16 +76,18 @@ const Home = ({setSelectedPage}: Props) => {
             >
               <p>Learn More</p>
             </AnchorLink>
-          </div>
+          </motion.div>
         </div>
 
         {/* IMAGE */}
-        <div>
+        <div className="flex basis-3/5 justify-center md:z-10
+              md:ml-40 md:mt-16 md:justify-end">
           <img src={HomePageGraphic} alt="home-page-graphic" />
         </div>
-      </div>
+      </motion.div>
 
       {/* SPONSORS */}
+      {isAboveMediumScreens && (
       <div className="flex flex-col justify-center items-center">
         <p className="text-sm font-bold text-gray-100">
           <span>
@@ -75,6 +100,7 @@ const Home = ({setSelectedPage}: Props) => {
           <img src={SponsorFortune} alt="sponsor-fortune" />
         </div>
       </div>
+      )}
     </section>
     )
 }
